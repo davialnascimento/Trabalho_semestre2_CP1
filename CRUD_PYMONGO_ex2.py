2baa0c81be411785fd5e9df947e96abf98d20d73
Davi Almeida Nascimento <davialnascimento@gmail.com>
2026-09-18 19:46:49 -0300

    Update CRUD_PYMONGO_ex2.py

--- CRUD_PYMONGO_ex2.py
+++ CRUD_PYMONGO_ex2.py
@@ -16,12 +16,57 @@ vulns = [
     {"cve_id": "CVE-2024-003", "tipo": "Path Traversal","severidade": "Critica","corrigida": False},
 ]
 
-def falhas_vivas(falha):
-    falha=vulns
-    if falha is True:
-        return falha
+vulns_true= [ {"cve_id": "CVE-2024-002", "tipo": "XSS",           "severidade": "Media", "corrigida": True}, ]
+
+#como eu posso fazer que seja checkado se é uma vulnerabilidade verdadeira ou falsa?
+cves_true = {vuln["cve_id"] for vuln in vulns_true}
+
+for vuln in vulns:
+    if vuln["cve_id"] in cves_true:
+        print(f"{vuln['cve_id']} -> Vulnerabilidade verdadeira")
     else:
-        return falha.remove()
+        print(f"{vuln['cve_id']} -> Vulnerabilidade falsa")
+
+vulnerabilidades.delete_many({})
+
+resultado = vulnerabilidades.insert_many(vulns)
+
+print(f"\n{len(resultado.inserted_ids)} vulnerabilidades inseridas.")
 
 
-#cve
+
+print("\nBuscar severidade='Alta'")
+
+resultado = vulnerabilidades.find({"severidade": "Alta"})
+
+for vuln in resultado:
+    print(f"{vuln['cve_id']}: {vuln['tipo']}")
+
+
+
+
+resultado = vulnerabilidades.update_one(
+    {"cve_id": "CVE-2024-001"},
+    {"$set": {"corrigida": True}}
+)
+
+print(f"\nUpdate corrigida=True em 001 -> "
+      f"{resultado.modified_count} documento modificado")
+
+
+
+quantidade = vulnerabilidades.count_documents(
+    {"corrigida": False}
+)
+
+print(f"\nCount corrigida=False -> {quantidade}")
+
+
+
+
+resultado = vulnerabilidades.delete_one(
+    {"cve_id": "CVE-2024-002"}
+)
+
+print(f"\nDelete CVE-2024-002 -> "
+      f"{resultado.deleted_count} documento removido")
